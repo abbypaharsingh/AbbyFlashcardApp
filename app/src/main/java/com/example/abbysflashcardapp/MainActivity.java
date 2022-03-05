642ffea433dbf9fbb1569d137cad2088943662d7
abbypaharsingh <arpaharsingh@gmail.com>
2022-03-05 01:36:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/abbysflashcardapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.abbysflashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView flashcardQuestion = findViewById(R.id.flashcard_question_textview);
+        TextView flashcardAnswer= findViewById(R.id.flashcard_answer_textview);
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+                // Do something here
+
+            }
+        });
+        flashcardAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.VISIBLE);
+                flashcardAnswer.setVisibility(View.INVISIBLE);
+                // Do something here
+
+            }
+        });
+    }
+}
